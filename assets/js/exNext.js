54a675a123cbc460fb6ea6cc640a637abac82feb
gustavo <gustavo.silva@sankhya.com.br>
2020-05-15 17:32:57 -0300

    exercícios

---
+++ assets/js/exNext.js
@@ -0,0 +1,18 @@
+function goToEx2() {
+  window.location.href = "./ex2.html";
+}
+function backToIndex() {
+  window.location.href = "./index.html";
+}
+function goToEx3() {
+  window.location.href = "./ex3.html";
+}
+function backToEx2() {
+  window.location.href = "./ex2.html";
+}
+function goToEx4() {
+  window.location.href = "./ex4.html";
+}
+function backToEx3() {
+  window.location.href = "./ex3.html";
+}
